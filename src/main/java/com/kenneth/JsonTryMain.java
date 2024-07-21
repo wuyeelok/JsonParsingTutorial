48448e922b437cc2b3992d5894214b190416c950
wuyeelok <wuyeelok@gmail.com>
2024-07-21 21:56:08 +0800

    Jackson will  serialize the Date to a timestamp format by default (number of milliseconds since January 1st, 1970, UTC).

--- src/main/java/com/kenneth/JsonTryMain.java
+++ src/main/java/com/kenneth/JsonTryMain.java
@@ -2,7 +2,13 @@ package com.kenneth;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.kenneth.jsonparsing.Json;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Locale;
 
 public class JsonTryMain {
     public static void main(String[] arg) {
@@ -19,5 +25,29 @@ public class JsonTryMain {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+
+        Date currentDate = new Date();
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+        String currentDateStr = "";
+        try {
+            currentDateStr = om.writeValueAsString(currentDate);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("It’s important to note that Jackson will serialize the Date to a timestamp format by default (number of milliseconds since January 1st, 1970, UTC).\n" +
+                "Jackson parse java util date will become: " + currentDateStr);
+
+        System.out.println("\n");
+
+        LocalDate ld = LocalDate.now();
+        String currentLocalDateStr = "";
+        try {
+            currentLocalDateStr = om.writeValueAsString(ld);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Jackson parse java time localDate will become: " + currentLocalDateStr);
+
     }
 }
